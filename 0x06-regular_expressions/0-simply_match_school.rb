4b0696985999dc6b5184047484f44427a38cd33e
Vincent Opurum <vincentchimene@gmail.com>
2023-10-05 16:20:25 +0100

    make the file executable

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,4 +1,4 @@
 #!/usr/bin/env ruby
-# A ruby script that accepts one argument and pass it to a regular expression matching method
+# A ruby script that accepts one argument and pass it to a regex matching method
 
 puts ARGV[0].scan(/school/i).join
